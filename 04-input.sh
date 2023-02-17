8f1f0aa9057c81a7fc25271188461034ea906fa3
Sivakumar S <sivakumarit42@gmail.com>
2023-02-17 16:19:40 +0530

    inputs in shell

---
+++ 04-input.sh
@@ -0,0 +1,12 @@
+# 0-n , $*/$@, $#
+
+echo First Argument $1 # First Argument
+echo Second Argument $2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+echo All Arguments = $* # all arguments
+echo All Arguments = $@ # All Arguments
+
+echo Number of Arguments = $# # Number of Arguments
+
