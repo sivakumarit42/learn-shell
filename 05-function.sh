1eb856a1589bb57c753582dfa82103860afa9997
Sivakumar S <sivakumarit42@gmail.com>
2023-02-18 14:30:49 +0530

    function in learn-shell

--- 05-function.sh
+++ 05-function.sh
@@ -1,10 +1,19 @@
 #declare a function
 
 xyz() {
-
-  echo hello world from function
+  echo Hello from Function
+  echo First Argument - $1
+  echo Second Argument -- $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
+  a=300
+  echo Value of a = $a
+  b=200
 }
 
-#main program
-#calling a function
-xyz
+## Main Program
+## Call a function
+a=120
+xyz 123 456
+echo Value of b - $b
+
