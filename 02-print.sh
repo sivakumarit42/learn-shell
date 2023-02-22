8793a111a789bb9bbdd308fa35a295ec018c0180
Sivakumar S <sivakumarit42@gmail.com>
2023-02-22 09:20:50 +0530

    changes

--- 02-print.sh
+++ 02-print.sh
@@ -18,3 +18,7 @@ echo Hello World
 echo -e "\e[31mHello in Red Color\e[0m"
 echo Hello World
 
+# Some times we need to print multiple lines
+
+echo -e "line1\nline2\nline3"
+## \e for color, same \n for new line
