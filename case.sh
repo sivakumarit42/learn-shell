b4beb39373c10800d34bd0da9f2ab43e60294037
Sivakumar S <sivakumarit42@gmail.com>
2023-02-22 15:25:32 +0530

    changes

---
+++ case.sh
@@ -0,0 +1,32 @@
+# case $var in
+# string1) commands ;;
+# string2) commands ;;
+# *) commands ;;
+# esac
+
+read -p 'Enter Course Name: ' name
+
+case $name in
+  DevOps)
+    echo Welcome to DevOps Training
+    echo Timings are 6AM
+    ;;
+  AWS)
+    echo Welcome to AWS Training
+    echo Timings are 7AM
+    ;;
+  *) echo No Course Available
+    ;;
+esac
+
+
+## same above in if condition
+if [ "${name}" == "DevOps" ]; then
+echo Welcome to DevOps Training
+echo Timings are 6AM
+elif [ "${name}" == "AWS"  ]; then
+  echo Welcome to AWS Training
+  echo Timings are 7AM
+else
+    echo No Course Available
+fi
---
+++ read_from_prompt
@@ -0,0 +1,7 @@
+# When we need a prompt for input
+
+read -p 'Enter your name: ' name
+
+echo "Your Name = $name"
+
+## Read command will fail for automation because manual intervention is required
