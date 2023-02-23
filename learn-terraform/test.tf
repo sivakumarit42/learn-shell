a63f3545f581cf9bde26268def5a1f9516033389
Sivakumar S <sivakumarit42@gmail.com>
2023-02-23 13:39:33 +0530

    changes

--- learn-terraform/test.tf
+++ learn-terraform/test.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "ec2" {
-  ami =ami-0a017d8ceb274537d
+  ami = "ami-0a017d8ceb274537d"
   instance_type ="t3.micro"
-  vpc_security_group_ids =[sg-0cbce48f1b39fa890]
+  vpc_security_group_ids =["sg-0cbce48f1b39fa890"]
   tags ={
     Name="test"
   }
 }
+
